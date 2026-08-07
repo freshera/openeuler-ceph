26cbd10b8b5912f66e57c5d3d6f1d638234aa7c1
zengkangsh <147360410@qq.com>
2026-08-07 20:58:08 +0800

    install-deps.sh 增加openeuler适配

--- install-deps.sh
+++ install-deps.sh
@@ -501,12 +501,16 @@ else
         $SUDO env DEBIAN_FRONTEND=noninteractive apt-get -y remove ceph-build-deps
         if [ "$control" != "debian/control" ] ; then rm $control; fi
         ;;
-    almalinux|rocky|centos|fedora|rhel|ol|virtuozzo)
+    almalinux|rocky|centos|openEuler|fedora|rhel|ol|virtuozzo)
         builddepcmd="dnf -y builddep --allowerasing"
         echo "Using dnf to install dependencies"
         case "$ID" in
             fedora)
                 $SUDO dnf install -y dnf-utils
+                ;;
+            openEuler)
+                MAJOR_VERSION="$(echo $VERSION_ID | cut -d. -f1)"
+                $SUDO dnf install -y dnf-utils selinux-policy-targeted
                 ;;
             almalinux|rocky|centos|rhel|ol|virtuozzo)
                 MAJOR_VERSION="$(echo $VERSION_ID | cut -d. -f1)"
--- openeuler_build.sh
+++ openeuler_build.sh
@@ -1,3 +1,15 @@
-##########################################
+##############################################
 ##在openeuler 24.03环境编译ceph，并制作出rpm包
-#########################################
+##############################################
+
+# 设置github代理
+export http_proxy=http://22.129.24.90:10808
+export https_proxy=http://22.129.24.90:10808
+
+# 下载ceph源码
+git clone https://github.com/freshera/openeuler-ceph
+cd openeuler-ceph
+git checkout v20.2.3
+
+# 安装依赖包（适配openeuler）
+./install-deps.sh
