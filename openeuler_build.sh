a0f5d5bbe25826d238aea3be1584e7a4ff62728e
zengkangsh <147360410@qq.com>
2026-08-07 21:40:03 +0800

    ceph源码安装针对openeuler完善，解决如下问题：  （1）jsonnet 和 python3-asyncssh 在openeuler不存在，在ceph.spec.in中进行适配； （2）openeuler的gcc-c++ 改为大于12.3

--- openeuler_build.sh
+++ openeuler_build.sh
@@ -10,6 +10,11 @@ export https_proxy=http://22.129.24.90:10808
 git clone https://github.com/freshera/openeuler-ceph
 cd openeuler-ceph
 git checkout v20.2.3
+git switch -c v20.2.3 origin/v20.2.3
+
+# 补齐python依赖（测试用）
+dnf install -y python3-pip
+pip3 install jsonnet asyncssh
 
 # 安装依赖包（适配openeuler）
 ./install-deps.sh
