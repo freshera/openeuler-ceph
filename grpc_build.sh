79c649e2ec0eda18a078bc513587f932ab40e282
haojie fu <hj_fu@foxmail.com>
2026-08-11 17:51:43 +0800

    add openeuler grpc build script

--- grpc_build.sh
+++ grpc_build.sh
@@ -1,12 +1,26 @@
 ########################################
-############编译打包grpc################
+############编译打包grpc#################
 ########################################
 
-# 1. 下载grpc源码
-git clone -b v1.60.0 --depth 1 https://github.com/gilfoylegpt/grpc.git
-cd grpc
-git switch -c v1.60.0
-git submodule update --init --recursive
-# git push --set-upstream origin refs/heads/v1.60.0
-# git pull --set-upstream origin refs/heads/v1.60.0
+# 1. 安装依赖
+dnf install -y rpm-build rpmdevtools cmake gcc-c++ \
+  protobuf-devel protobuf-compiler protobuf-lite-devel \
+  openssl-devel c-ares-devel gtest-devel zlib-devel gperftools-devel \
+  re2-devel abseil-cpp-devel \
+  python3-devel python3-setuptools python3-Cython
 
+# 2. 下载源码,替换spec文件
+rm -rf ~/rpmbuild 
+cd ~
+rpmdev-setuptree
+dnf download --source grpc
+rpm -ivh grpc-1.60.0-*.src.rpm
+rm -rf ~/rpmbuild/SPECS/grpc.spec
+cd -
+cp ./grpc.spec ~/rpmbuild/SPECS/
+
+# 3. 编译打包
+rpmbuild -ba ~/rpmbuild/SPECS/grpc.spec
+
+# 4. 检查cmake
+rpm -qlp ~/rpmbuild/RPMS/$(uname -m)/grpc-devel-1.60.0-5.1*.rpm | grep gRPCConfig
