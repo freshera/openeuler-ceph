19da69ea936c47dc4757d1cffcbd374c788b5e6f
haojie fu <hj_fu@foxmail.com>
2026-08-13 11:24:23 +0800

    fix ceph version bug

--- openeuler_build.sh
+++ openeuler_build.sh
@@ -105,12 +105,12 @@ cp -r openeuler-ceph ceph-srpm
 cd ceph-srpm
 export http_proxy=http://22.129.24.90:10808
 export https_proxy=http://22.129.24.90:10808
-./make-srpm.sh 20.3.2
+./make-srpm.sh 20.2.3
 
 # 编译出rpm安装包
 rm -rf /root/rpmbuild/*
 mkdir -p /root/rpmbuild/{SPECS,SOURCES}
-cp ceph-v20.3.2.tar.bz2  /root/rpmbuild/SOURCES
+cp ceph-v20.2.3.tar.bz2  /root/rpmbuild/SOURCES
 cp ceph.spec  /root/rpmbuild/SPECS
 export http_proxy=http://22.129.24.90:10808
 export https_proxy=http://22.129.24.90:10808
